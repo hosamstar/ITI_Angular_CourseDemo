50eb5fd5a98bf56eaac5b3d0b165054afca34083
hosam shaker <hossamcosper@gmail.com>
2023-05-07 18:10:01 +0300

    Finish day 1 => Introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // template: '<h1>Hello World {{ title }}</h1>',
+  styleUrls: ['./app.component.scss'],
+  // styles: ['h1{color: brown;}'],
 })
 export class AppComponent {
   title = 'courseDemo';
